6cc24c82002f0f757d3d427b0959f8529843e3a3
ovitindev <victorhugomarquezin@gmail.com>
2023-01-16 16:20:23 -0300

    Traslate the login and register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -14,28 +14,28 @@
                                 <img src="{{ URL::to('assets/images/logo-full.png') }}" alt="">
                             </a>
                         </div>
-                        <h4 class="text-center mb-4">Sign in your account</h4>
+                        <h4 class="text-center mb-4 mt-4">Entre na sua conta</h4>
                         <form action="{{ route('login') }}" method="POST">
                             @csrf
                             <div class="mb-3">
-                                <label class="mb-1"><strong>Username</strong></label>
+                                <label class="mb-1"><strong>Login</strong></label>
                                 <input type="text" class="form-control @error('username') is-invalid @enderror" name="username">
                             </div>
                             <div class="mb-3">
-                                <label class="mb-1"><strong>Password</strong></label>
+                                <label class="mb-1"><strong>Senha</strong></label>
                                 <input type="password" class="form-control @error('password') is-invalid @enderror" name="password">
                             </div>
                             <div class="row d-flex justify-content-between mt-4 mb-2">
                                 <div class="mb-3">
-                                    <a href="#">Forgot Password?</a>
+                                    <a href="#">Esqueceu sua senha?</a>
                                 </div>
                             </div>
                             <div class="text-center">
-                                <button type="submit" class="btn btn-primary btn-block">Sign Me In</button>
+                                <button type="submit" class="btn btn-primary btn-block">Entrar</button>
                             </div>
                         </form>
                         <div class="new-account mt-3">
-                            <p>Don't have an account? <a class="text-primary" href="{{ route('register') }}">Sign up</a></p>
+                            <p>Ainda não tem uma conta? <a class="text-primary" href="{{ route('register') }}">Adquira o nosso sistema</a></p>
                         </div>
                     </div>
                 </div>
